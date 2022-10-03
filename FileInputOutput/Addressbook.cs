1f8d500d5d581df565a982869c4c1aaaab6dc203
AbhiShinde1 <abhishinde572@gmail.com>
2022-10-03 16:41:32 +0530

    Added program using File Input OutPut

---
+++ FileInputOutput/Addressbook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInputOutput
+{
+    public class Addressbook
+    {
+        private static string path = @"C:\Users\Shree\Desktop\Day27_File_IO\FileInputOutput\Demo.txt";
+        public static void ReadFromStreamReader()
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = " ";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+        public static void WriteUsingStreamReader()
+        {
+
+            using (StreamWriter sr = File.AppendText(path))
+            {
+               
+                sr.WriteLine(" Firstname = Abhishek, Lastname = jagtap, Contact number = 727669541, ZipCode = 411028 ");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+    }
+}
---
+++ FileInputOutput/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FileInputOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Input Output Program");
+            Addressbook.ReadFromStreamReader();
+            Addressbook.WriteUsingStreamReader();
+        }
+    }
+}
